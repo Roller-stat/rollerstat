6a535bcab5892646b2424c36b8970003da055706
asadsk8r02 <asadkhanworkmail@gmail.com>
2025-10-08 16:07:22 -0500

    fix metatags warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,11 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
-  viewport: "width=device-width, initial-scale=1",
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default async function RootLayout({
